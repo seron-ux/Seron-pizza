091b43105d0f40a0f49b424be684d1380dfe2c5f
seron-ux <abyseronkirui@gmail.com>
2021-01-09 14:55:05 +0300

    form a pizzaFlavour in js

---
+++ script.js
@@ -0,0 +1,10 @@
+var pizzaFlavour = [
+    "Bacon",
+    "sausage",
+    "chicken",
+    "Black olives",
+    "Turkish Ground"
+]
+var pizzaSize =["Regular", "Medium","Large"]
+var pizzaCrust=["Thin crust","Thick crust","Deep crust"
+var pizzaTopping =["Mushroom","onion","Berry"]

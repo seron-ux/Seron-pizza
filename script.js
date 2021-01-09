fbd1d22f379d765e6b8a2ca5c574447a213b81df
seron-ux <abyseronkirui@gmail.com>
2021-01-09 15:46:03 +0300

    called the functions

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ var pizzaFlavour = [
 var pizzaSize =["Regular", "Medium","Large"]
 var pizzaCrust=["Thin crust","Thick crust","Deep crust"
 var pizzaTopping =["Mushroom","onion","Berry"]
+
+function getFlavour() {
+    var flavour =$("#pizzaflavour :selected").val();
+    return parseInt(flavour);
+}
+function getSize() {
+   var size = $("#pizzasize :selected").val();
+   return parseInt(size);
+}

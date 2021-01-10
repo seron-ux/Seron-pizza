54b957bfc1d3ad9569c3fe031177f0fd752a28b9
seron-ux <abyseronkirui@gmail.com>
2021-01-10 11:48:41 +0300

    added a copywright form

--- script.js
+++ script.js
@@ -36,26 +36,11 @@ function getCrust() {
         parseInt(crust.val()) +
         parseInt(topping.val())) *
       parseInt(number.val());
-    alert(
-      "You've ordered " + 
-        number.val() +  
-        " "  +
-        "pizza's" + 
-        " "  +
-        flavour.html() +
-        " "  + 
-        size.html() +
-        " "  +
-        crust.html() +
-        " "  + 
-        topping.html() +
-        " "  +
-        " which amounts to Ksh. "  +
-        results +
-        " Thanks for your order!"
-    );
-    prompt("Enter your location");
-    prompt("Enter your name");
-    prompt("Enter your phone number");
-    alert("Your order will be delivered in a while, delivery fee is  Ksh.100/=");
-  }
+      function getAmount(flavour, size, crust, topping, number) {
+        var results =
+          (parseInt(flavour.val()) +
+            parseInt(size.val()) +
+            parseInt(crust.val()) +
+            parseInt(topping.val())) *
+          parseInt(number.val());
+        
